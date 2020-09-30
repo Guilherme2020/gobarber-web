177dcfc5148594104b3dccb2333dd6f6b5c3249d
Guilherme Rodrigues Simeão <guilhermerodrigues123477@gmail.com>
2020-09-30 14:32:34 -0300

    (feat) config pages, signin, signup

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Form } from '@unform/web';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu Cadastro</h1>
+
+          <Input icon={FiUser} name="name" placeholder="Nome" />
+
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
+
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para o Login
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
